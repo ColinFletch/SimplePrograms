83ae95113fa027f5b70355fee67d36b4c8073b30
ColinFletch <cfletch@us.ibm.com>
2015-10-25 22:44:29 -0400

    First Commit

---
+++ src/com/colinsorg/windchill/WindChill.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.colinsorg.windchill;
+
+/**
+ * @author cfletch
+ *
+ */
+/******************************************************************************
+ *  Compilation:  javac WindChill.java
+ *  Execution:    java WindChill t v
+ *
+ *  Given the temperature t (in Fahrenheit) and the wind speed v
+ *  (in miles per hour), compute the wind chill w using the formula
+ *  from the National Weather Service.
+ *
+ *     w = 35.74 + 0.6215*t + (0.4275*t - 35.75)  *  v ^ 0.16
+ *
+ *  Reference:  http://www.nws.noaa.gov/om/windchill/index.shtml
+ *
+ ******************************************************************************/
+
+public class WindChill {
+    public static void main(String[] args) {
+        double t = Double.parseDouble("30.00");
+        double v = Double.parseDouble("15.00");
+        double w = 35.74 + 0.6215*t + (0.4275*t - 35.75) * Math.pow(v, 0.16);
+        System.out.println("Temperature = " + t);
+        System.out.println("Wind speed  = " + v);
+        System.out.println("Wind chill  = " + w);
+    }
+
+}
